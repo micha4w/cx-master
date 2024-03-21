aa0c2b18237863bf4ca3d0167aace5db9ef8d576
micha4w <micha4w@gmail.com>
2024-03-21 09:30:52 +0100

    chore: improve build

--- extension/src/vite-env.d.ts
+++ extension/src/vite-env.d.ts
@@ -1,3 +1,9 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
 /// <reference types="@samrum/vite-plugin-web-extension/client" />
+
+declare global {
+    const CX_DEBUG : boolean;
+}
+
+export {};
--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -4,7 +4,8 @@ import webExtension from "vite-plugin-web-extension";
 import path from "path";
 import { getManifestChrome, getManifestFirefox } from "./src/manifest";
 
-const browser = process.env.CX_BROWSER ?? 'firefox'
+const browser = process.env.CX_BROWSER ?? 'firefox';
+const debug = process.env.CX_DEBUG && (process.env.CX_DEBUG === 'true' || Number.parseInt(process.env.CX_DEBUG) > 0);
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -16,6 +17,13 @@ export default defineConfig(() => {
         manifest: browser === 'chrome' ? getManifestChrome : getManifestFirefox,
       }),
     ],
+    define: {
+      CX_DEBUG: debug,
+    },
+    mode: debug ? 'development' : 'production',
+    build: {
+      minify: !debug,
+    },
     worker: {
       // To allow for dynamic importing in your injected.ts?url&worker TODO
       format: "es" as const
