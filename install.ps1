2bc6a6f9632de1a7ff5b9bb0d49b81cbaf4d9403
micha4w <micha4w@gmail.com>
2024-03-22 16:27:26 +0100

    install: fix paths

--- install.ps1
+++ install.ps1
@@ -7,15 +7,20 @@ $DOWNLOAD = "v1.2.3/cx-lsp-controller-x86_64-pc-windows-msvc.exe"
 $MANIFEST = '{
   "name": "' + $NATIVE_NAME + '",
   "description": "Bridge to connect Code Expert with local LSPs Servers",
-  "path": "' + $Env:APPDATA + '/cx-master/cx-lsp-controller",
+  "path": "' + $Env:APPDATA + '\cx-master\cx-lsp-controller",
   "type": "stdio",
   "allowed_extensions": ["cx-master@micha4w.ch"],
   "allowed_origins": ["chrome-extension://fdmghidnemaceleocaolmgdkpegkhlcf/"]
 }'
 
+$MANIFEST_LOCATIONS = @(
+  "HKCU:\Software\Google\Chrome\NativeMessagingHosts",
+  "HKCU:\Software\Mozilla\NativeMessagingHosts" 
+)
+
 Write-Host "Downloading Executable..."
 New-Item -ItemType Directory -Path "$Env:APPDATA/cx-master" -Force
-Invoke-WebRequest "https://github.com/$REPO/releases/download/$DOWNLOAD" -OutFile "$Env:APPDATA/cx-master/cx-lsp-controller"
+Invoke-WebRequest "https://github.com/$REPO/releases/download/$DOWNLOAD" -OutFile "$Env:APPDATA\cx-master\cx-lsp-controller"
 
 Write-Host "Downloading Repository..."
 $tmp = New-TemporaryFile
@@ -25,16 +30,21 @@ Write-Host "Extracting Zip Configs..."
 $zip = [ZipFile]::OpenRead($tmp)
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 $zip.Entries.Where{ $_.FullName -match "^[^/]*-main/native-host/lsps/." -and $_.FullName -notmatch "/$" }.ForEach{
-    $newFile = [IO.FileInfo]($_.FullName -replace "^[^/]*-main/native-host/", "$Env:APPDATA/cx-master/");
-    $newFile.Directory.Create();
-    [ZipFileExtensions]::ExtractToFile($_, $newFile);
+  $newFile = [IO.FileInfo]($_.FullName -replace "^[^/]*-main/native-host/", "$Env:APPDATA\cx-master\")
+  $newFile.Directory.Create()
+  $newFile.Delete()
+  [ZipFileExtensions]::ExtractToFile($_, $newFile)
 }
 $zip.Dispose()
 $tmp | Remove-Item
 
 Write-Host "Creating Manifest..."
-New-Item "$Env:APPDATA" -ItemType File -Value $MANIFEST
+New-Item "$Env:APPDATA\cx-master\$NATIVE_NAME.json" -ItemType File -Value $MANIFEST -Force
 
 Write-Host "Registering Manifest..."
-New-ItemProperty -Path "HKCU\Software\Google\Chrome\NativeMessagingHosts" -Name $NATIVE_NAME -Value "$Env:APPDATA/cx-master/$NATIVE_NAME.json"
-New-ItemProperty -Path "HKCU\Software\Mozilla\NativeMessagingHosts" -Name $NATIVE_NAME -Value "$Env:APPDATA/cx-master/$NATIVE_NAME.json"
+foreach ($LOCATION in $MANIFEST_LOCATIONS) {
+  if(!(Test-Path $LOCATION)){
+    New-Item $LOCATION -Force;
+  }
+  New-ItemProperty -Path $LOCATION -Name $NATIVE_NAME -Value "$Env:APPDATA\cx-master\$NATIVE_NAME.json" -Force
+}
